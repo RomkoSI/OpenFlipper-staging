7b5ab11de64328a54942a66c3645f1454b3a5e50
Christopher Tenter <christopher.tenter@rwth-aachen.de>
2013-08-21 14:32:45 +0000

    Add fxaa PostProcessor plugin
    git-svn-id: http://www.openflipper.org/svnrepo/OpenFlipper/branches/Free-Staging@17363 383ad7c9-94d9-4d36-a494-682f7c89f535

---
+++ PluginCollection-PostProcessors/Plugin-FXAA/PostProcessorFXAAPlugin.hh
@@ -0,0 +1,98 @@
+/*===========================================================================*\
+*                                                                            *
+*                              OpenFlipper                                   *
+*      Copyright (C) 2001-2011 by Computer Graphics Group, RWTH Aachen       *
+*                           www.openflipper.org                              *
+*                                                                            *
+*--------------------------------------------------------------------------- *
+*  This file is part of OpenFlipper.                                         *
+*                                                                            *
+*  OpenFlipper is free software: you can redistribute it and/or modify       *
+*  it under the terms of the GNU Lesser General Public License as            *
+*  published by the Free Software Foundation, either version 3 of            *
+*  the License, or (at your option) any later version with the               *
+*  following exceptions:                                                     *
+*                                                                            *
+*  If other files instantiate templates or use macros                        *
+*  or inline functions from this file, or you compile this file and          *
+*  link it with other files to produce an executable, this file does         *
+*  not by itself cause the resulting executable to be covered by the         *
+*  GNU Lesser General Public License. This exception does not however        *
+*  invalidate any other reasons why the executable file might be             *
+*  covered by the GNU Lesser General Public License.                         *
+*                                                                            *
+*  OpenFlipper is distributed in the hope that it will be useful,            *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU LesserGeneral Public           *
+*  License along with OpenFlipper. If not,                                   *
+*  see <http://www.gnu.org/licenses/>.                                       *
+*                                                                            *
+\*===========================================================================*/
+
+/*===========================================================================*\
+*                                                                            *
+*   $Revision: 17080 $                                                       *
+*   $LastChangedBy: moeller $                                                *
+*   $Date: 2013-07-19 12:58:31 +0200 (Fri, 19 Jul 2013) $                     *
+*                                                                            *
+\*===========================================================================*/
+
+
+#include <QObject>
+
+#include <OpenFlipper/BasePlugin/BaseInterface.hh>
+#include <OpenFlipper/BasePlugin/PostProcessorInterface.hh>
+
+#include <ACG/GL/globjects.hh>
+#include <ACG/ShaderUtils/GLSLShader.hh>
+
+class PostProcessorFXAAPlugin : public QObject, BaseInterface, PostProcessorInterface
+{
+   Q_OBJECT
+   Q_INTERFACES(BaseInterface)
+   Q_INTERFACES(PostProcessorInterface)
+
+#if QT_VERSION >= 0x050000
+  Q_PLUGIN_METADATA(IID "org.OpenFlipper.Plugins.Plugin-PostProcessorDepthImage")
+#endif
+
+  public:
+   PostProcessorFXAAPlugin();
+   ~PostProcessorFXAAPlugin();
+  
+  public :
+    QString name() { return (QString("FXAA Postprocessor Plugin")); };
+    QString description( ) { return (QString(tr("Fast approximate anti aliasing"))); };
+
+    
+  public slots:
+    QString version() { return QString("1.0"); };
+    
+  private slots:
+
+    void updateDepthStencilTextureBuffer(ACG::GLState* _glstate);
+
+    void postProcess(ACG::GLState* _glstate);
+
+    QString postProcessorName();
+
+  private:
+    /// depthStencil texture buffer
+    ACG::Texture2D pSceneTexture_;
+
+    /// Current width of the depthStencil texture buffer
+    int     sceneTexWidth_;
+
+    /// Current height of the depthStencil texture buffer
+    int     sceneTexHeight_;
+
+
+    /// fxaa shader
+    GLSL::Program* fxaa_;
+
+  private:
+};
+
