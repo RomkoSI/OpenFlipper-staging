77a86a3845e114b21c396419a16acd27ef1245e0
Jan Möbius <moebius@cs.rwth-aachen.de>
2013-09-03 14:38:28 +0000

    Fixed bad commit
    git-svn-id: http://www.openflipper.org/svnrepo/OpenFlipper/branches/Free-Staging@17469 383ad7c9-94d9-4d36-a494-682f7c89f535

--- PluginCollection-PostProcessors/Plugin-FXAA/Shaders/FXAA/screenquad.glsl
+++ PluginCollection-PostProcessors/Plugin-FXAA/Shaders/FXAA/screenquad.glsl
@@ -8,13 +8,3 @@ void main()
 	gl_Position = inPosition;
 	vTexCoord = inPosition.xy * 0.5 + vec2(0.5, 0.5);
 }
-#version 150
-
-in vec4 inPosition;
-out vec2 vTexCoord;
-
-void main()
-{
-	gl_Position = inPosition;
-	vTexCoord = inPosition.xy * 0.5 + vec2(0.5, 0.5);
-}
