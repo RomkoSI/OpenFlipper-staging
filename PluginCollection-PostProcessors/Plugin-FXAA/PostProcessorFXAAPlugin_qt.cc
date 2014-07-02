d5d3dd3b88e27f47e54bb9cd8a2e457a7384a4f4
Jan Möbius <moebius@cs.rwth-aachen.de>
2014-07-02 08:11:07 +0000

    Doxygen errors
    git-svn-id: http://www.openflipper.org/svnrepo/OpenFlipper/branches/Free-Staging@19051 383ad7c9-94d9-4d36-a494-682f7c89f535

--- PluginCollection-PostProcessors/Plugin-FXAA/PostProcessorFXAAPlugin_qt.cc
+++ PluginCollection-PostProcessors/Plugin-FXAA/PostProcessorFXAAPlugin_qt.cc
@@ -40,9 +40,10 @@
 *                                                                            *
 \*===========================================================================*/
 
-/**
- * @file Contains definitions that require qt headers which are incompatible
- * with glew.h.
+/** @file
+ *
+ *  Contains definitions of the PostProcessorFXAAPlugin that require qt headers
+ *  which are incompatible with glew.h.
  */
 
 #include "PostProcessorFXAAPlugin.hh"
